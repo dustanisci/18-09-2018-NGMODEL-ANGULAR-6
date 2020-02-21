1370b27978f39a3853d094184973817029fef7bc
Eduardo <eduardo.stanisci@omnibees.com>
2020-02-21 10:01:58 -0300

    template-driven forms

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('form-ngmodel app is running!');
+    expect(page.getTitleText()).toEqual('template-driven-forms app is running!');
   });
 
   afterEach(async () => {
